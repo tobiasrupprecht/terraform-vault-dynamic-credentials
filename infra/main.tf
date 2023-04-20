802601a8af6942477921edf22c0e5589a236891a
tobiasrupprecht <trupprecht@hashicorp.com>
2023-04-20 17:32:37 +0200

    included ec instance

--- infra/main.tf
+++ infra/main.tf
@@ -26,6 +26,7 @@ provider "aws" {
   #Below is an example of using K/V Secrets to configure this AWS Provider
   #access_key = data.vault_kv_secret_v2.creds.data["AWS_ACCESS_KEY"]
   #secret_key = data.vault_kv_secret_v2.creds.data["AWS_SECRET_ACCESS_KEY"]
+
   #Below is the same configuration to configure the AWS Provider using the dynamically created credentials
   access_key = data.vault_aws_access_credentials.awsdynamiccreds.access_key
   secret_key = data.vault_aws_access_credentials.awsdynamiccreds.secret_key
@@ -39,10 +40,10 @@ output "regions" {
   value = data.aws_regions.current.names
 }
 
-#resource "aws_instance" "instance" {
-#  instance_type = "t3.micro"
-#  ami = "ami-0110d1b5b1cdd8780"  
-#}
+resource "aws_instance" "instance" {
+  instance_type = "t3.micro"
+  ami = "ami-0110d1b5b1cdd8780"  
+}
 
 
 
