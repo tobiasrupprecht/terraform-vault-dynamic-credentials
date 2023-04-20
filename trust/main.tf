0252f9f94424bd0fd3354ec176555f1ded7b8976
Cedric Feist <cedric.feist@gmail.com>
2023-04-20 10:51:47 +0200

    updates

--- trust/main.tf
+++ trust/main.tf
@@ -74,26 +74,4 @@ path "aws/*" {
 EOT
 }
 
-resource "vault_aws_secret_backend" "aws" {
-  access_key = "AKIA....."
-  secret_key = "AWS secret key"
-}
 
-resource "vault_aws_secret_backend_role" "role" {
-  backend = vault_aws_secret_backend.aws.path
-  name    = "tfc"
-  credential_type = "iam_user"
-
-  policy_document = <<EOT
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "iam:*", "ec2:*"
-      "Resource": "*"
-    }
-  ]
-}
-EOT
-}
